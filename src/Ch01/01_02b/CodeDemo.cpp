a87bf4959d10ca45ba81d127b730cda5ced974f2
Andres Guapi <46389486+oguapi@users.noreply.github.com>
2025-02-19 17:35:02 +0000

    Hello world exercise

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,14 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+//Library
+#include <iostream>
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+
+    //2 Space to separate my message from the debugger's exit
+    std::cout << std::endl << std::endl;
+    return 0;  //Mostly cero represent all ok
+}
