83380eb8cd18b3c7df4042e1c1f9023d9ddd38e2
Andres Guapi <46389486+oguapi@users.noreply.github.com>
2025-02-21 16:22:04 +0000

    Asking and presenting by console

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,10 +3,16 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
+//This code just help with simple input without space
 int main(){
+    std::string name;
     std::cout << "Hi There!" << std::endl;
+    std::cout << "Enter your name: " << std::flush; //flush makes sure that the output is completely sent to the terminal before waiting for the user input
+    std::cin >> name;
 
+    std::cout << "Nice to meet you, " << name << "!" << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
